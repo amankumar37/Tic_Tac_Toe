67dc8b4eedd93c4209bf39f24d87220224b09c4a
redgem <redgem@code.com>
2023-01-23 13:43:16 +0000

    Files ready to use

---
+++ script.js
@@ -0,0 +1,67 @@
+
+let turn = 'X';
+let color=[1,1.5,2,2.5,3,3.5,4,4.5,5,5.5,6,6.5,7,7.5,8,8.5,9,9.5];
+
+//funtion for changing turn
+const  changeTurn=()=>{
+          if(turn=='X') turn='0';
+          else turn ='X';
+          document.getElementById("status").innerText="Turn for " + turn;
+}
+
+let boxes=document.getElementsByClassName('boxText');
+
+
+// function to check win
+
+let wins=[
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+];
+
+function checkWin(){
+    
+    for(let i=0;i<wins.length;i++){
+        if((boxes[wins[i][0]].innerText===boxes[wins[i][1]].innerText)&&(boxes[wins[i][1]].innerText===boxes[wins[i][2]].innerText)&&(boxes[wins[i][0]].innerText)!==""){
+               return true;
+        }
+    }
+    return false;
+}
+
+
+// Logic
+
+
+
+
+for(let i=0;i<boxes.length;i++){
+   boxes[i].addEventListener('click',()=>{
+    if(boxes[i].innerText==""){
+        boxes[i].innerText=turn;
+        if(checkWin()){
+            document.getElementById('status').innerText=turn + " won";
+        }
+        changeTurn();
+
+    }
+   });
+}
+
+
+
+document.getElementById('reset').addEventListener('click',function(){
+
+    var rand=color[Math.floor(Math.random() * color.length)]*100;
+    document.querySelector(':root').style.setProperty('--hue',rand);
+    for(let i=0;i<boxes.length;i++){
+        boxes[i].innerText="";
+    }
+    document.getElementById('status').innerText="Turn for X";
+});
